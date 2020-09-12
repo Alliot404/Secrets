407b291aad957798d7d94951fc29d8fb8008f619
abhilash <abhilashramu1999@gmail.com>
2020-09-12 19:41:13 +0530

    add session passport passport-local-mongoose

--- app.js
+++ app.js
@@ -3,29 +3,49 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+// const md5 = require("md5"); not using
 // const encrypt = require("mongoose-encryption"); not using
-const md5 = require("md5");
+
 
 const app = express();
-
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
+userSchema.plugin(passportLocalMongoose);
 
  // const secret = process.env.API_KEY;//creating secrete const //// Now this is not using
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']}); // adding encrypt plugin before userSchema model to encrypt only password
 
 const User = new mongoose.model("User", userSchema);
 
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/",function(req,res){
   res.render("home");
@@ -39,45 +59,90 @@ app.get("/register",function(req,res){
   res.render("register");
 });
 
+app.get("/secrets",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("secrets");
+  }
+  else{
+    res.redirect("/login");
+  }
+});
+
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+});
+
 ////new user document database with mongoose when user registers
 app.post("/register", function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(err){
-      res.send(err);
-    }
-    else{
-      res.render("secrets");
-    }
-  });
+
+User.register({username: req.body.username}, req.body.password, function(err, user){
+  if(err){
+    console.log(err);
+    res.redirect("/register");
+  }
+  else{
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/secrets");
+    });
+  }
+});
+  ////********not using below previous code because now using passport.js
+  // const newUser = new User({
+  //   email: req.body.username,
+  //   password: md5(req.body.password)
+  // });
+  // newUser.save(function(err){
+  //   if(err){
+  //     res.send(err);
+  //   }
+  //   else{
+  //     res.render("secrets");
+  //   }
+  // });
 });
 
 app.post("/login", function(req,res){
-  const username = req.body.username;   //// user entering into login form
-  const password = md5(req.body.password);
 
-///verifying user login credentials with our DB
-  User.findOne({email: username}, function(err,foundUser){
-    if(err){
-      console.log(err);
-    }
-    else if(!foundUser){
-      res.sendFile(__dirname+ "/failure.html");
-    }
-    else{
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
-        else{
-          res.sendFile(__dirname+ "/failure.html");
-        }
-      }
-    }
-  });
+const user = new User({
+  username:req.body.username,
+  password:req.body.password
+});
+
+req.login(user, function(err){
+  if(err){
+    console.log(err);
+  }
+  else{
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/secrets");
+    });
+  }
+});
+
+  ////********not using below previous code because now using passport.js
+//   const username = req.body.username;   //// user entering into login form
+//   const password = md5(req.body.password);
+//
+// ///verifying user login credentials with our DB
+//   User.findOne({email: username}, function(err,foundUser){
+//     if(err){
+//       console.log(err);
+//     }
+//     else if(!foundUser){
+//       res.sendFile(__dirname+ "/failure.html");
+//     }
+//     else{
+//       if(foundUser){
+//         if(foundUser.password === password){
+//           res.render("secrets");
+//         }
+//         else{
+//           res.sendFile(__dirname+ "/failure.html");
+//         }
+//       }
+//     }
+//   });
 });
 app.post("/failure", function(req,res){ //// from failure page redirecting to login page
   res.redirect("/login");
