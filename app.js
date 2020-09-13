30173e8b97699866f12bf2d3b777940a3c638701
abhilash <abhilashramu1999@gmail.com>
2020-09-13 15:28:50 +0530

    add google oauth

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+
+const GoogleStrategy = require( 'passport-google-oauth20' ).Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 // const md5 = require("md5"); not using
 // const encrypt = require("mongoose-encryption"); not using
 
@@ -19,7 +22,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.use(session({
-  secret: "Our little secret.",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false
 }));
@@ -32,9 +35,11 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
 });
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
  // const secret = process.env.API_KEY;//creating secrete const //// Now this is not using
 // userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']}); // adding encrypt plugin before userSchema model to encrypt only password
@@ -44,12 +49,56 @@ const User = new mongoose.model("User", userSchema);
 // use static authenticate method of model in LocalStrategy
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
   res.render("home");
 });
+
+// app.get("/auth/google",function(req,res){
+//   passport.authenticate('google', { scope: ["profile"]});
+// });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'https://www.googleapis.com/auth/plus.login',
+      , 'https://www.googleapis.com/auth/plus.profile.emails.read' ] }
+));
+
+app.get( "/auth/google/secrets",
+    passport.authenticate( 'google', { failureRedirect: "/login"}),
+    function(res,res){
+      res.redirect("/secrets");
+    });
+
+// app.get( "/auth/google/secrets",
+//     passport.authenticate( 'google', {
+//         successRedirect: '/auth/google/secrets',
+//         failureRedirect: '/auth/google/login'
+// }));
+
 
 app.get("/login",function(req,res){
   res.render("login");
